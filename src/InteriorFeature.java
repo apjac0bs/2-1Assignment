d22e0203ac9f7d0b857c254993a06a47d0a6a8a3
Alex Jacobs <36943751+apjac0bs@users.noreply.github.com>
2018-12-08 14:22:17 -0500

    Added comments to make program more user friendly

--- src/InteriorFeature.java
+++ src/InteriorFeature.java
@@ -1,13 +1,17 @@
+// Create a concrete class named InteriorFeature that implements the interface Feature with the following:
+
+
 public class InteriorFeature implements Feature {
 
-  private String interiorFeature;
+  private String interiorFeature; // Create a String named interiorFeature as an instance variable
 
-  public InteriorFeature() {
-    setInteriorFeature("Generic");
+  public InteriorFeature() {  // A public default constructor without parameters 
+    setInteriorFeature("Generic"); // that sets the interiorFeature instance variable to “Generic”
   }
 
-  public InteriorFeature(String interiorFeature) {
-    setInteriorFeature(interiorFeature);
+  public InteriorFeature(String interiorFeature) { // An overloaded constructor with the following value:
+    setInteriorFeature(interiorFeature);           // A String with a parameter value of interiorFeature
+  // Set the interiorFeature instance variable to the parameter interiorFeature
   }
 
   private void setInteriorFeature(String interiorFeature) {
@@ -15,17 +19,18 @@ public class InteriorFeature implements Feature {
   }
 
   @Override
-  public String getFeature() {
+  public String getFeature() { // A public method named getFeature with no parameters and returns an instance of String
     return this.interiorFeature;
   }
 
   @Override
-  public void setFeature(String feature) {
-    setInteriorFeature(feature);
+  public void setFeature(String feature) { // public method named setFeature that takes a string parameter interiorFeature
+    setInteriorFeature(feature);           // and returns a void. 
+  // should set the instance variable interiorFeature
   }
 
   @Override
-  public String toString() {
+  public String toString() { // A public toString method that returns the following: Interior [Generic]
     return "Interior [" + getFeature() + "]";
   }
 }

d83e7fafab9fe9ea45931ebce1bfee38d81268a7
Alex Jacobs <36943751+apjac0bs@users.noreply.github.com>
2018-12-08 15:00:19 -0500

    Added comments to make program more user friendly

--- src/VehicleFrame.java
+++ src/VehicleFrame.java
@@ -1,22 +1,24 @@
+// Create a concrete class named VehicleFrame that implements the interface Chassis with the following:
+
 public class VehicleFrame implements Chassis {
 
-  private String vehicleFrameType;
+  private String vehicleFrameType; // Create a String named vehicleFrameType instance variable
 
-  public VehicleFrame(){
-    setChassisType("Unibody");
+  public VehicleFrame(){ // default public constructor
+    setChassisType("Unibody"); // set vehicle frame instance using "unibody" as default
   }
 
-  public VehicleFrame(String vehicleFrameType){
-    setVehicleFrameType(vehicleFrameType);
+  public VehicleFrame(String vehicleFrameType){ // overloaded constructor with parameter vehicleFrameType
+    setVehicleFrameType(vehicleFrameType); // set vehicle frame instance 
   }
 
   @Override
-  public Chassis getChassisType()  {
+  public Chassis getChassisType()  { // public method named getChassisType with no parameters and returns instance of Chassis
     return this;
   }
 
   @Override
-  public void setChassisType(String vehicleFrameType) {
+  public void setChassisType(String vehicleFrameType) { // public method setChassisType takes String vehicleFrameType parameter
     setVehicleFrameType(vehicleFrameType);
   }
 
@@ -25,10 +27,10 @@ public class VehicleFrame implements Chassis {
   }
 
   @Override
-  public String toString() {
+  public String toString() { // A public toString method that returns the following:
     if (this.vehicleFrameType.equals("Unibody")){
-      return "Chassis : " + VehicleFrame.chassis +
-          "\nVehicle Frame : " + this.vehicleFrameType;
+      return "Chassis : " + VehicleFrame.chassis + // Chassis : Chassis
+          "\nVehicle Frame : " + this.vehicleFrameType; // Vehicle Frame : Unibody
     }
     else {
       return "Vehicle Frame : " + this.vehicleFrameType;

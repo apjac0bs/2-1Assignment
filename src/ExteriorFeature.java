44f9762b1316ad839c1c2caa0cbbf615adf4c87b
Alex Jacobs <36943751+apjac0bs@users.noreply.github.com>
2018-12-08 13:57:45 -0500

    Added comments to make program more user friendly

--- src/ExteriorFeature.java
+++ src/ExteriorFeature.java
@@ -1,27 +1,29 @@
+// Create a concrete class named ExteriorFeature that implements the interface Feature with the following:
+
 public class ExteriorFeature implements Feature{
 
-  private String exteriorFeature;
+  private String exteriorFeature; // create a string named ExteriorFeature as an instance variable
 
-  public ExteriorFeature() {
+  public ExteriorFeature() { // public default constructor that sets the exterior feature instance variable
     setFeature("Generic");
   }
 
-  public ExteriorFeature(String exteriorFeature) {
-    setFeature(exteriorFeature);
+  public ExteriorFeature(String exteriorFeature) { // overloaded constructor with String with parameter value: exteriorFeature
+    setFeature(exteriorFeature);                   // set the exteriorFeature and use features as String values
   }
 
   @Override
-  public String getFeature() {
+  public String getFeature() { // public method named getFeature with no parameters and returns instance of String.
     return this.exteriorFeature;
   }
 
   @Override
-  public void setFeature(String feature) {
-    this.exteriorFeature = feature;
+  public void setFeature(String feature) { // public method named setFeature that takes a String parameter named exteriorFeature
+    this.exteriorFeature = feature;        // returns a void and sets instance variable exteriorFeature
   }
 
   @Override
-  public String toString() {
+  public String toString() { // A public toString method that returns the following: Exterior [Generic]
     return "Exterior [" + getFeature() + "]";
   }
 }

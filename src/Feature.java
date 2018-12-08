888e9d6fbf6f6e5d42e58ad7ba2d40a2cc5168e2
Alex Jacobs <36943751+apjac0bs@users.noreply.github.com>
2018-12-08 14:08:05 -0500

    Added comments to make program more user friendly

--- src/Feature.java
+++ src/Feature.java
@@ -1,3 +1,7 @@
+// Create an interface Feature with the following method definitions:
+// public String getFeature();
+// public void setFeature(String feature);
+
 public interface Feature {
 
   public String getFeature();

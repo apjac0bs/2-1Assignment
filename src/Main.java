8612d756c0c3bb362ad798cb2933a53d8fe75647
Alex Jacobs <36943751+apjac0bs@users.noreply.github.com>
2018-12-08 19:28:43 -0500

    Added comments to make program more user friendly

--- src/Main.java
+++ src/Main.java
@@ -13,38 +13,44 @@ public class Main {
   }
 
   public static void testVehicleChassis() {
-    // Creates two objects, one with the default constructor
-    // and the other with the constructor with parameters.
-    // Give sample data for the parameters.
+    
+    
+    // sample data is given for parameters
 
-    VehicleChassis myChassis = new VehicleChassis();
+    VehicleChassis myChassis = new VehicleChassis(); // Creates two objects, one with the default constructor
     System.out.println(myChassis.toString());
 
-    VehicleChassis yourChassis = new VehicleChassis("cn");
+    VehicleChassis yourChassis = new VehicleChassis("cn"); // constructor with parameters
     System.out.println(yourChassis.toString());
   }
 
   public static void testVehicleFrame() {
-    // Step 4 Test
+    // Write a static main method that tests two scenarios (for step 4)
     VehicleFrame vf1 = new VehicleFrame();
     System.out.println(vf1);
 
-    VehicleFrame vf2 = new VehicleFrame("Ladder Frame");
+    VehicleFrame vf2 = new VehicleFrame("Ladder Frame"); // prints a value set by a single string value: Vehicle Frame : Ladder Frame
     System.out.println(vf2);
   }
-
+  // Write a static main method that tests two scenarios:
   public static void testManufacturedEngine() {
-    ManufacturedEngine me1 = new ManufacturedEngine();
+    ManufacturedEngine me1 = new ManufacturedEngine(); // One that prints a generic set of strings
     System.out.println(me1);
 
     ManufacturedEngine me2 =
-        new ManufacturedEngine("Honda", new Date()
+        new ManufacturedEngine("Honda", new Date() // One that accepts call parameters and returns the following:
             , "H-Series", "H23A1"
             , "2WD: Two-Wheel Drive", 4
             , "88 AKI");
     System.out.println(me2);
   }
-
+  // Write a static main method that tests two scenarios:
+  // One that prints all fuel grade values, like: Interior [Generic]
+  // Another like: Interior [Climate Control]
+  // Write a static main method that tests two scenarios:
+  // One that prints all fuel grade values, like: Exterior [Generic]
+  // Another like: Exterior [Fog Lamps]
+  
   public static void testFeatures() {
     InteriorFeature if1 = new InteriorFeature();
     System.out.println(if1);
@@ -59,12 +65,14 @@ public class Main {
     System.out.println(ef2);
 
   }
-
-  public static void testVehicle() {
+  // Write a static main method that tests two scenarios:
+  // One that prints a generic set of strings,
+  
+  public static void testVehicle() { 
     Vehicle v1 = new Vehicle();
     System.out.println(v1);
 
-    Vehicle v2 = new Vehicle(new Date(), "Honda", "Prelude", "Coupe",
+    Vehicle v2 = new Vehicle(new Date(), "Honda", "Prelude", "Coupe", // One that accepts call parameters and returns the following:
         new VehicleChassis("Unibody"),
         "null", "type",
         new ManufacturedEngine("Honda", new Date(), "H-series",
